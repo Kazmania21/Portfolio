87945b2cbb8bdfa9f98bb676738a257bbe8a5342
Kazmania21 <91277965+Kazmania21@users.noreply.github.com>
2023-03-20 07:41:40 -0400

    Java Fix

--- Timeline/5 - Summer (2022)/Java/FirstJavaProject/MemberLinks.java
+++ Timeline/5 - Summer (2022)/Java/FirstJavaProject/MemberLinks.java
@@ -1,18 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class MemberLinks {
     public static void main (String[] args){
         System.out.print("What is the streamer's name: ");
-        Scanner nameInput = new Scanner(System.in);
-        String name = nameInput.nextLine();
-        nameInput.close();
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
         
-        String [] = "", "";
+        String [] socials = {"YouTube", "Twitch", "DLive", "Sliver", "Twitter", "Instagram", "SnapChat", "Facebook", "Spotify", "TikTok"};
+        List<String> inputs = new ArrayList<>();
+        
+        for (String social : socials){
+            System.out.print(String.format("What is their %s?\n", social));
 
-        int questionLoopNumber = 0;
-
-        while (questionLoopNumber < 10){
-            System.out.print("What is their %s", );
+            String link = input.nextLine();
+            inputs.add(link);
         };
+        input.close();
+
+        System.out.println("\nUGN " + name);
+
+        List<String> labels = Arrays.asList("🔴 YOUTUBE | ", "🔴 TWITCH | ", "🔴 DLIVE | ", "🔴 SLIVER | ", "💻 TWITTER | ", "📲 INSTAGRAM | ", "📲 SNAPCHAT | ",
+        "😜 FACEBOOK | ", "🎶 SPOTIFY : ", "🕑 TIKTOK | ");
+
+        ListIterator<String> label = labels.listIterator();
+        while (label.hasNext()) {
+            String nextInput = inputs.get(label.nextIndex());
+            if (nextInput != ""){
+                System.out.println(String.format("%s %s", label.next(), nextInput));
+            }
+        }
     }
 }
