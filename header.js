09bb1debe78ee526e9763e4a35c7ddd27edf46e8
Kazmania21 <91277965+Kazmania21@users.noreply.github.com>
2023-10-25 13:08:55 -0400

    New Pages Added

---
+++ header.js
@@ -0,0 +1,9 @@
+const header_xhttp = new XMLHttpRequest()
+
+header_xhttp.onload = function() {
+    const body = document.body
+    body.innerHTML = this.responseText + body.innerHTML
+}
+
+header_xhttp.open("GET", "./header.html")
+header_xhttp.send()
